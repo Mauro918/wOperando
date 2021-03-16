7b34a6ab9d594ca9c423ad3dea65857e8a8a043f
Mauro918 <lunatycbmx@gmail.com>
2021-03-16 11:18:04 -0500

    Agregar archivos de proyecto.

---
+++ Forms1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace wOperando
+{
+    partial class frmOperando
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumeros = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumeros
+            // 
+            this.lblNumeros.AutoSize = true;
+            this.lblNumeros.Location = new System.Drawing.Point(104, 85);
+            this.lblNumeros.Name = "lblNumeros";
+            this.lblNumeros.Size = new System.Drawing.Size(147, 13);
+            this.lblNumeros.TabIndex = 0;
+            this.lblNumeros.Text = "Ingrese 2 numeros decimales ";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(327, 85);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(542, 85);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 2;
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(351, 309);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnEjecutar.TabIndex = 3;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // frmOperando
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNumeros);
+            this.Name = "frmOperando";
+            this.Text = "OperacionesBasicas";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmOperando_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumeros;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnEjecutar;
+    }
+}
+
---
+++ Forms1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wOperando
+{
+    public partial class frmOperando : Form
+    {
+        public frmOperando()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Numero1 = double.Parse(txtNum1.Text);
+                double Numero2 = double.Parse(txtNum2.Text);
+
+                MessageBox.Show("La suma es: " + Sumando.Suma.Sumar(Numero1, Numero2) + "\n" + "La resta es: " + Restando.Resta.Restar(Numero1, Numero2)
+                + "\n" + "La multiplicacion  es: " + Multiplicando.Multiplicacion.Multiplica(Numero1, Numero2)
+                + "\n" + "La divicion es: " + Dividiendo.Divicion.Dividir(Numero1, Numero2)
+                + "\n" + "La raiz del numero " + Numero1 + "  es: " + SacadoRaiz.RaizCuadrada.Raiz(Numero1, Numero2)
+                + "\n" + "La raiz del numero " + Numero2 + "  es: " + SacadoRaiz.RaizCuadrada.Raizb(Numero1, Numero2));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Verifique que los datos ingresados sean numeros ");
+
+            }
+           
+
+        }
+
+        private void frmOperando_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show(" Realmente desea salir?", " Comfirmacion ",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            e.Cancel = (resultado == DialogResult.No);
+        }
+    }
+}
